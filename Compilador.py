8b70254177074cedec4b3a881178deefbbbec561
matheusalmeida-dev <msalmeida.dev@gmail.com>
2021-08-21 12:44:07 -0300

    Refina o analisador léxico

--- Compilador.py
+++ Compilador.py
@@ -1,14 +1,19 @@
 import csv
 
 
-linha = 'while i < 100 do i = i + j;'
+linha = 'while ; i < 10i0 do i = i + j;'
 
 reservadas = ['while', 'do']
 operadores = ['<', '=', '+']
 terminador = ';'
 identificadores = ['i', 'j']
+numeros = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
-
+def valida(item):
+    for letra in item:
+        if not letra in numeros:
+            return False
+    return True
 def procura(item):
     if item in reservadas:
         return 'Palavra reservada'
@@ -18,8 +23,10 @@ def procura(item):
         return 'Terminador'
     elif item in identificadores:
         return 'Identificador'
-    else:
+    elif item in numeros or valida(item):
         return 'Constante'
+    else:
+        return 'ERRO'
 
 def pesquisa(token, lista):
     for item in lista:
@@ -27,18 +34,37 @@ def pesquisa(token, lista):
             return True
     return False
 
+def formata(linha):
+    itens = linha.split(' ')
+    for i, item in enumerate(itens):
+        if procura(item) == 'ERRO':
+            itens[i] = item.replace('j', ' j ').replace('i', ' i ').replace(';', ' ; ').replace('+', ' + ').replace('=', ' = ').replace('do', ' do ').replace('while', ' while ').replace('<', ' < ')
+    lista = ' '.join(itens).split(' ')
+    lista = [x for x in lista if x]
+    return lista
+
+def verifica(item):
+    if item in reservadas or item in operadores or item in terminador or item in identificadores or valida(item):
+        return True
+    return False
+
 def traduz(linha):
-    itens = linha.replace(';', '').split(' ')
+    itens = formata(linha)
+    print(itens)
     arq = open('tabelas.csv', 'w', newline='', encoding='utf-8')
     w = csv.writer(arq)
     w.writerow(['TOKEN', 'IDENTIFICACAO', 'TAMANHO', 'POSICAO'])
 
     listaIdent = []
     listaAtual = []
+    
     for i in range(len(itens)):
         token = itens[i]
+        if not verifica(itens[i]):
+            print('ERRO! Elemento "{}" não conhecidos pelo analisador léxico!'.format(token))
+            break
         identificacao = procura(itens[i])
-        if identificacao == 'Constante' or identificacao == 'Identificador':
+        if identificacao == 'Identificador' or identificacao == 'Constante':
             if not pesquisa(token, listaIdent):
                 listaIdent.append([token, identificacao])
                 identificacao = [identificacao, len(listaIdent)]
@@ -58,8 +84,9 @@ def traduz(linha):
             w.writerow(["'"+token, identificacao, tamanho, '(0,{})'.format(posicao)])
         else:
             w.writerow([token, identificacao, tamanho, '(0,{})'.format(posicao)])
-    w.writerow([linha[-1], 'Terminador', 1, '(0,{})'.format(len(linha)-1)])
-    w.writerow([])
+    arq.close()
+    arq = open('simbolos.csv', 'w', newline='', encoding='utf-8')
+    w = csv.writer(arq)
     w.writerow(['INDICE', 'SiMBOLO'])
     for item in listaIdent:
         w.writerow([listaIdent.index(item)+1, item[0]])
